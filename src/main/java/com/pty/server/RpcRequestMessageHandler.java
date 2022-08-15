6ea7b2bcf0c30f6b8be6f54f59ab31e8b0d8129e
penty7710 <1219002685@qq.com>
2022-08-15 23:10:04 +0800

    获取方法的返回值

--- src/main/java/com/pty/server/RpcRequestMessageHandler.java
+++ src/main/java/com/pty/server/RpcRequestMessageHandler.java
@@ -2,11 +2,15 @@ package com.pty.server;
 
 import com.pty.message.RpcRequestMessage;
 import com.pty.message.RpcResponseMessage;
+import com.pty.registry.impl.NacosServerRegistryImpl;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @author : pety
@@ -15,14 +19,30 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RpcRequestMessageHandler extends SimpleChannelInboundHandler<RpcRequestMessage> {
 
+    //服务注册
+    private static NacosServerRegistryImpl serverRegistry;
+
+    static {
+        serverRegistry = new NacosServerRegistryImpl();
+    }
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, RpcRequestMessage msg) throws Exception {
         log.info("服务端接收到请求:{}",msg);
-        String interfaceName = msg.getInterfaceName();
-        //通过调用对应的方法得到结果 TODO
-        Object result = null;
-        ChannelFuture future = ctx.writeAndFlush(RpcResponseMessage.success(result));
-        future.addListener(ChannelFutureListener.CLOSE);
+        ChannelFuture future = null;
+        try {
+            String interfaceName = msg.getInterfaceName();
+            //获取到对应的服务
+            Object service = serverRegistry.getService(interfaceName);
+            //反射调用方法得到返回值
+            Method method = service.getClass().getMethod(msg.getMethodName(), msg.getParameterTypes());
+            Object result = method.invoke(service, msg.getParameterValue());
+            ctx.writeAndFlush(RpcResponseMessage.success(result));
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e ) {
+            log.error("远程调用出错："+e);
+            ctx.writeAndFlush(RpcResponseMessage.fail(e));
+        }finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 }
